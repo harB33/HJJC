722c5de6f52960a78f824d0f9df40cf563c75756
harB33 <bautistaharvy@gmail.com>
2025-10-25 09:51:18 +0800

    Initial Commit

---
+++ src/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/daisyui@5"
+        rel="stylesheet"
+        type="text/css"
+    />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="./style/output.css" />
+</head>
+<body class="w-screen overflow-x-hidden">
+    <div class="sticky top-0 z-50 ">
+        <?php include './components/header.html'; ?>
+    </div>
+    <section class="flex flex-col items-center text-center w-full">
+        <section id="title" class="text-3xl font-light h-[80vh] w-full uppercase flex flex-col justify-center items-center my-fadeOutText">
+            <h1 class="font-giaza font-black text-8xl mb-4">HJJC. Store</h1>
+            <h1 class=" font-light text-4xl">Your one-stop shop for <span class="font-bold underline italic">everything</span> you need!</h1>
+            <h1 class="font-extralight text-2xl">Scroll down to discover who we are and why customers love us.</h1>
+        </section>
+        <section class="flex flex-col h-screen w-screen justify-center items-center">
+            <h1 class=" text-5xl font-bold my-fadeInText m-6 uppercase">Why Shop With Us?</h1>
+            <div class="flex gap-8  justify-center items-center">
+                <div class="card bg-base-100 w-96 shadow-sm my-fadeInCard">
+                    <figure>
+                        <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shop Smart. Shop Authentic.</h2>
+                    </div>
+                </div>
+                <div class="card bg-base-100 w-96 shadow-sm my-fadeInCard">
+                    <figure>
+                        <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Trusted by Thousands, Chosen by You.</h2>
+                    </div>
+                </div>
+                <div class="card bg-base-100 w-96 shadow-sm my-fadeInCard">
+                    <figure>
+                        <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Real Products. Real Value. Real Trust.</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="relative w-full overflow-clip flex ">
+            <img src="./image/shop-now.png" alt="circle" class="w-1/2 my-moveIn"/>
+            <img src="./image/shop-now.png" alt="circle" class="w-1/2 my-moveIn"/>
+            <img src="./image/shop-now.png" alt="circle" class="w-1/2 my-moveIn"/>
+            <img src="./image/shop-now.png" alt="circle" class="w-1/2 my-moveIn"/>
+            <img src="./image/shop-now.png" alt="circle" class="w-1/2 my-moveIn"/>
+            <img src="./image/shop-now.png" alt="circle" class="w-1/2 my-moveIn"/>
+            <img src="./image/shop-now.png" alt="circle" class="w-1/2 my-moveIn"/>
+        </section>
+        <section class="mt-50">
+            <h1 class="text-4xl font-bold m-6 uppercase my-fadeInText my-popUp">Find it all here. Explore our wide range of products built for you.</h1>
+            <div class="grid grid-cols-[auto_auto] grid-rows-2 gap-4 place-content-center ">
+                <div class="border rounded-2xl h-152 w-85 row-span-2 bg-[url(../image/gpu.png)] bg-cover my-fadeInCard">1</div>
+                <div class="border rounded-2xl h-73 w-150 bg-[url(../image/shoes.png)] bg-contain my-fadeInCard"></div>
+                <div class="border rounded-2xl h-73 w-150 bg-[url(../image/laptop.png)] bg-contain my-fadeInCard">3</div>
+            </div>
+        </section>
+        <img src="./image/circle.png" alt="circle" class="size-150 absolute -left-50 -bottom-40 z-0 my-circle"/>
+        <img src="./image/circle.png" alt="circle" class="size-250 absolute -top-95 -right-95 my-circle z-0 "/>
+    </section>
+</body>
+</html>
