b20fd2fa425c8c8485842fd3399e098d137d8e98
harB33 <bautistaharvy@gmail.com>
2025-10-28 03:58:58 +0800

    updated register with alert message for user and email already existing

--- src/register.php
+++ src/register.php
@@ -1,3 +1,68 @@
+<?php
+include("./db/db.php");
+date_default_timezone_set('Asia/Manila');
+
+$alert_html_output = userAndEmailAlert();
+
+function userAndEmailAlert(){
+    include("./db/db.php");
+    $alertMsg = '';
+
+    if (isset($_POST["register"])) {
+        $user = filter_input(INPUT_POST, "user", FILTER_SANITIZE_SPECIAL_CHARS);
+        $pass = filter_input(INPUT_POST, "pass", FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_SPECIAL_CHARS);
+    
+        $hashed_pass = password_hash($pass, PASSWORD_DEFAULT);
+    
+        date_default_timezone_set('Asia/Manila');
+        $date = date("Y-m-d H:i:s");
+    
+        $sql = "SELECT * FROM users WHERE username='$user' OR email='$email'";
+        $res = $conn->query($sql);
+    
+        if ($res->num_rows > 0) {
+            $row = $res->fetch_assoc();
+    
+            if ($row['username'] === $user) {
+                $alertMsg .= '
+                    <div role="alert" class="alert alert-warning">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                        </svg>
+                        <span>Warning: User Already Exists!</span>
+                    </div>';
+            }
+            if ($row['email'] === $email) {
+                $alertMsg .= '
+                    <div role="alert" class="alert alert-warning">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                        </svg>
+                        <span>Warning: Email Already Exists!</span>
+                    </div>';
+            }
+        } else {
+            $sql = "INSERT INTO users (username, password, email, created_at)
+                    VALUES ('$user', '$hashed_pass', '$email', '$date')";
+    
+            if ($conn->query($sql)) {
+                $alertMsg .= '
+                    <div role="alert" class="alert alert-success">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <span>Your Account has been Created!</span>
+                    </div>';
+            } else {
+                die("Error: " . $conn->error);
+            }
+        }
+        $conn->close();
+    }
+    return $alertMsg;
+}
+?>
 <!DOCTYPE html>
 <html lang="en" data-theme="light">
 
@@ -63,7 +128,6 @@
                         title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
                         name="pass" />
                 </label>
-
                 <label class="input validator input-lg rounded-full w-3/4">
                     <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <g
@@ -79,56 +143,16 @@
                     <input type="email" name="email" placeholder="mail@site.com" required />
                 </label>
                 <input type="submit" class="btn rounded-full w-3/4 btn-lg  border text-[20px]" name="register" value="REGISTER">
-            </form>
-            <a href="./login.php" class="hover:underline">Already Have an Account? Login</a>
-        </div>
+                </form>
+                <a href="./login.php" class="hover:underline">Already Have an Account? Login</a>
+                <div class=" w-fit gap-2 flex-col flex min-h-30">
+                    <?php
+                        echo $alert_html_output;
+                    ?>
+                </div>
+            </div>
         <div></div>
     </div>
 </body>
 
 </html>
-
-
-<?php
-
-include("./db/db.php");
-
-date_default_timezone_set('Asia/Manila');
-
-if (isset($_POST["register"])) {
-    $user = filter_input(INPUT_POST, "user", FILTER_SANITIZE_SPECIAL_CHARS);
-    $pass = filter_input(INPUT_POST, "pass", FILTER_SANITIZE_SPECIAL_CHARS);
-    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_SPECIAL_CHARS);
-
-    $hashed_pass = password_hash($pass, PASSWORD_DEFAULT);
-
-    date_default_timezone_set('Asia/Manila');
-    $date = date("Y-m-d H:i:s");
-
-    $sql = "SELECT * FROM users WHERE username='$user' OR email='$email'";
-    $res = $conn->query($sql);
-
-    if ($res->num_rows > 0) {
-        $row = $res->fetch_assoc();
-
-        if ($row['username'] === $user) {
-            die("Username already exists.");
-        }
-        if ($row['email'] === $email) {
-            die("Email already exists.");
-        }
-    } else {
-        $sql = "INSERT INTO users (username, password, email, created_at)
-                VALUES ('$user', '$pass', '$email', '$date')";
-
-        if ($conn->query($sql)) {
-            header("Location: ./login.php");
-            exit();
-        } else {
-            die("Error: " . $conn->error);
-        }
-    }
-}
-
-$conn->close();
-?>
