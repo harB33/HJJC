e286b6cdf86436d0d698ebe17670a0f4263f173c
harB33 <bautistaharvy@gmail.com>
2025-11-23 04:34:32 +0800

    minor update

--- src/cart.php
+++ src/cart.php
@@ -125,7 +125,7 @@ $_SESSION['total'] = $total;
     <section class="flex flex-col min-h-screen h-full w-full justify-start items-center lg:pt-20 max-lg:pt-10 bg-custom-background">
         <h1 class=" max-lg:text-3xl lg:text-5xl font-extrabold text-custom-text/80 w-full text-center max-lg:py-10 lg:py-15">CHECK OUT</h1>
         <div class="fixed max-lg:top-[6%] max-lg:left-[4%] lg:top-[10%] lg:left-[8%] z-40">
-            <a href="./home.php" class="btn btn-circle shadow-none bg-custom-accent border-none">
+            <a href="./home.php" class="btn btn-circle shadow-none bg-custom-accent/20 border-custom-accent border hover:bg-custom-accent duration-300">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left-icon lucide-arrow-left">
                     <path d="m12 19-7-7 7-7" />
                     <path d="M19 12H5" />
@@ -319,26 +319,8 @@ $_SESSION['total'] = $total;
                 </div>
             <?php else: ?>
             <?php endif; ?>
-
         </div>
     </section>
-    <!-- <section class=" w-full items-center justify-center flex  fixed bottom-0 p-4 bg-custom-background shadow-2xl">
-        <?php if (!empty($cart_items)): ?>
-            <div class="flex gap-4 grow w-1/2 items-center justify-center">
-                <form method="POST" action="./orders.php" class="grow w-full">
-                    <input type="hidden" name="product_id" value="<?= $product['product_id']; ?>">
-                    <button type="submit" class="btn btn-lg border-custom-accent bg-custom-accent text-custom-background w-full rounded-full  text-sm">Buy Now</button>
-                </form>
-            </div>
-            <div class=" w-1/2 grow items-end justify-center flex flex-col">
-                <div>
-                    <h1 class="text-sm font-medium w-full float-right flex justify-between">Qty: <span><?php echo $total_quantity ?></span> </h1>
-                    <h1 class="text-sm font-medium w-full float-right ">Total</h1>
-                    <p class="text-2xl font-bold w-full text-custom-accent">₱<?= number_format($total, 2); ?></p>
-                </div>
-            </div>
-        <?php endif; ?>
-    </section> -->
     <section class="w-full  items-center justify-center flex fixed bottom-0 p-4 bg-custom-background shadow-2xl">
         <?php if (!empty($cart_items)): ?>
             <form method="POST" action="./orders.php" class="flex gap-4 max-w-2xl grow w-full items-center justify-center">
