f411602001b8647b2bb8e2dea846a8b7811959a4
harB33 <bautistaharvy@gmail.com>
2026-05-17 09:04:32 +0800

    section

--- views/components/header.php
+++ views/components/header.php
@@ -14,15 +14,15 @@ require_once __DIR__ . '/../../public/handlers/searchbar.php';
 </head>
 
 <body data-theme="light" class="">
-    <header class="fixed top-0 w-screen flex justify-center z-50 items-center lg:px-5 lg:py-2 max-lg:px-2.5 max-lg:py-0.5  backdrop-blur-2xl">
+    <header class="fixed top-0 w-screen flex justify-center z-50 items-center lg:px-5 lg:py-2 max-lg:px-2.5 max-lg:py-0.5 backdrop-blur-xl">
         <div class="flex max-w-7xl w-full items-center justify-between">
             <a href="<?php echo BASE_URL; ?>/" class="grow"><img src="<?php echo ASSET_URL; ?>/images/logo/Coffee_Logo.png" alt="logo" class="size-12" /></a>
             <!-- <div class="flex items-center gap-2 w-full justify-end md:justify-center"> -->
             <div class="flex items-center justify-between grow-2">
-                <a href="<?php echo BASE_URL; ?>/" class="text-2xl font-urbanist italic font-light text-zinc-100 border border-transparent hover:border-custom-accent transition-all duration-300">Home</a>
-                <a href="<?php echo BASE_URL; ?>/home" class="text-2xl font-urbanist italic font-light text-zinc-100 border border-transparent hover:border-custom-accent transition-all duration-300">Menu</a>
-                <a href="#" class="text-2xl font-urbanist italic font-light text-zinc-100 border border-transparent hover:border-custom-accent transition-all duration-300">Contact</a>
-                <a href="#" class="text-2xl font-urbanist italic font-light text-zinc-100 border border-transparent hover:border-custom-accent transition-all duration-300">About Us</a>
+                <a href="<?php echo BASE_URL; ?>/" class="text-2xl font-urbanist italic font-light text-zinc-100 border-b  border-transparent hover:border-custom-accent transition-all duration-300 text-shadow-lg">Home</a>
+                <a href="<?php echo BASE_URL; ?>/home" class="text-2xl font-urbanist italic font-light text-zinc-100 border-b  border-transparent hover:border-custom-accent transition-all duration-300">Menu</a>
+                <a href="#" class="text-2xl font-urbanist italic font-light text-zinc-100 border-b  border-transparent hover:border-custom-accent transition-all duration-300">Contact</a>
+                <a href="#" class="text-2xl font-urbanist italic font-light text-zinc-100 border-b  border-transparent hover:border-custom-accent transition-all duration-300">About Us</a>
             </div>
             <div class="flex items-center justify-end grow gap-2">
                 <!-- <form action="/product" method="get" class="h-[65%] w-full">
