9f824d778078c3132ec61e4ef31803f16d635351
harB33 <bautistaharvy@gmail.com>
2026-05-20 21:29:45 +0800

    minor

--- views/components/header.php
+++ views/components/header.php
@@ -48,7 +48,7 @@ require_once __DIR__ . '/../../public/handlers/searchbar.php';
                     </label>
                 </form> -->
                 <a href="<?php echo BASE_URL; ?>/cart" class=" text-2xl font-urbanist italic font-medium hover:text-custom-accent active:text-custom-accent  duration-300"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-8.5 stroke-zinc-100 hover:stroke-custom-accent"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" /></svg></a>
-                <div class="dropdown dropdown-hover dropdown-center group">
+                <div class="dropdown dropdown-hover dropdown-end group">
                     <div tabindex="0" role="button" class="group-hover:text-custom-accent hover:text-custom-accent active:text-custom-accent duration-300">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-8.5 stroke-zinc-100 group-hover:stroke-custom-accent hover:stroke-custom-accent"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" /></svg></div>
                     <ul tabindex="-1" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
--- views/pages/cart.php
+++ views/pages/cart.php
@@ -279,15 +279,15 @@ $_SESSION['total'] = $total;
                         <div class="relative flex flex-col w-full h-full max-w-sm">
                             <input type="radio" name="paymentMethod" id="cod-radio" class="hidden peer" value="cod">
                             <label
-                                for="cod-radio" class="w-full h-full cursor-pointer border border-transparent rounded-2xl duration-300 peer-checked:border-[#e69c4d]">
-                                <div class="w-full h-full bg-custom-background rounded-2xl peer-checked:bg-[#e69c4d]">
+                                for="cod-radio" class="w-full h-full cursor-pointer border border-transparent rounded-2xl duration-300 peer-checked:bg-custom-accent">
+                                <div class="w-full h-full bg-custom-background rounded-2xl peer-checked:bg-custom-accent">
                                     <div
-                                        class="w-full text-center text-black/75 h-full bg-custom-accent/15  text-xl font-black border-custom-accent/0 border-2 rounded-2xl duration-300   flex items-end justify-center pb-2.5  peer-checked:ring-2 peer-checked:ring-[#e69c4d] peer-checked:border-2   peer-checked:text-[#f9f6f3] peer-checked:bg-transparent">
+                                        class="w-full text-center text-black/75 h-full bg-custom-accent/15  text-xl font-black border-custom-accent/0 border-2 rounded-2xl duration-300   flex items-end justify-center pb-2.5  peer-checked:ring-2 peer-checked:ring-custom-accent peer-checked:border-2   peer-checked:text-custom-background peer-checked:bg-transparent">
                                         CASH ON DELIVERY
                                     </div>
                                 </div>
                                 <span class="absolute inset-y-0 top-[30%] -translate-y-[50%] left-[50%] -translate-x-[50%] flex h-full items-center text-gray-400 pointer-events-none peer-checked:text-white">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-wallet size-[90%] stroke-custom-accent peer-checked:stroke-[#f9f6f3]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-wallet size-[90%] stroke-custom-accent peer-checked:stroke-custom-background">
                                         <path d="M19 7V4a1 1 0 0 0-1-1H5a2 2 0 0 0 0 4h15a1 1 0 0 1 1 1v4h-3a2 2 0 0 0 0 4h3a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1" />
                                         <path d="M3 5v14a2 2 0 0 0 2 2h15a1 1 0 0 0 1-1v-4" />
                                     </svg>
@@ -303,15 +303,15 @@ $_SESSION['total'] = $total;
                                 value="cc">
                             <label
                                 for="cc-radio"
-                                class="w-full h-full cursor-pointer border border-transparent rounded-2xl duration-300 peer-checked:border-[#e69c4d]">
-                                <div class="w-full h-full bg-custom-background rounded-2xl peer-checked:bg-[#e69c4d]">
+                                class="w-full h-full cursor-pointer border border-transparent rounded-2xl duration-300 peer-checked:bg-custom-accent">
+                                <div class="w-full h-full bg-custom-background rounded-2xl peer-checked:bg-custom-accent">
                                     <div
-                                        class="w-full text-center text-black/75 h-full text-xl bg-custom-accent/15 font-black border-none rounded-2xl duration-300   flex items-end justify-center pb-2.5  peer-checked:ring-2 peer-checked:ring-[#e69c4d]    peer-checked:text-[#f9f6f3] peer-checked:bg-transparent">
+                                        class="w-full text-center text-black/75 h-full text-xl bg-custom-accent/15 font-black border-none rounded-2xl duration-300   flex items-end justify-center pb-2.5  peer-checked:ring-2 peer-checked:ring-custom-accent    peer-checked:text-custom-background peer-checked:bg-transparent">
                                         CREDIT CARD
                                     </div>
                                 </div>
                                 <span class="absolute inset-y-0 top-[30%] -translate-y-[50%] left-[50%] -translate-x-[50%] flex h-full items-center text-gray-400 pointer-events-none peer-checked:text-white">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-credit-card size-[90%] stroke-custom-accent peer-checked:stroke-[#f9f6f3]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-credit-card size-[90%] stroke-custom-accent peer-checked:stroke-custom-background">
                                         <rect width="20" height="14" x="2" y="5" rx="2" />
                                         <line x1="2" x2="22" y1="10" y2="10" />
                                     </svg>
@@ -353,4 +353,4 @@ $_SESSION['total'] = $total;
 
 </html>
 
-<script src="<?php echo ASSET_URL; ?>/js/quantity.js"></script>
+<script src="<?php echo ASSET_URL; ?>/js/quantity.js"></script>
